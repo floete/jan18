088ddc1c2406c35c28e199be7d8b3d1c5faabd2b
floete <bemmannqiq@gmail.com>
2014-01-18 17:09:37 +0900

    value changed

--- sample.c
+++ sample.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
+int f()
+{
+	printf("f\n");
+	return 1;
+}
+
+
+int g()
+{
+	printf("g\n");
+	return 0;
+}
+
+
 int main()
 {
 	int a = 2;
 	int b = 15;
-	int c = (a | b);
-	int d = (a || b);
+	int c = ( f() | g() );
+	int d = ( f() || g() );
 	
 	printf("a = %d\n",a);
 	printf("b = %d\n",b);
